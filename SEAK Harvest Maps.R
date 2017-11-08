dcd0736f15394c413d77b01a05172e3d484de7da
krshedd <kyle.shedd@alaska.gov>
2017-11-08 12:41:02 -0900

    Create 5-year average map for D1-4.

--- SEAK Harvest Maps.R
+++ SEAK Harvest Maps.R
@@ -17,7 +17,7 @@ setwd("V:/Analysis/1_SEAK/Sockeye/Mixture/Harvest Data")
 
 # Get OceanAK Harvest data
 # So far I just have Sockeye 2014-2016
-file <- "Southeast Sockeye Salmon Catch by Day and Stat Area 2016.csv"
+file <- "Southeast Sockeye Salmon Catch by Day and Stat Area 2017.csv"
 harvest <- read.csv(file = file, as.is = TRUE)
 str(harvest)
 
@@ -70,8 +70,40 @@ SEAK_StatArea_Harvest_Fishery.mat <- data.matrix(frame = SEAK_StatArea_Harvest_F
 rownames(SEAK_StatArea_Harvest_Fishery.mat) <- SEAK_StatArea_Harvest_Fishery.df[, 1]
 SEAK_StatArea_Harvest_Fishery.mat <- cbind(SEAK_StatArea_Harvest_Fishery.mat, "Commercial" = rowSums(SEAK_StatArea_Harvest_Fishery.mat))
 str(SEAK_StatArea_Harvest_Fishery.mat)
-max(SEAK_StatArea_Harvest_Fishery.mat)  # 2014: 265,810; 2015: 323,013; 2016: 212,374
+max(SEAK_StatArea_Harvest_Fishery.mat)  # 2013: 117,291; 2014: 265,810; 2015: 323,013; 2016: 212,374; 2017: 134,053
 
+
+
+## Compute five year average for 2013-2017
+# Save years separately
+SEAK_StatArea_Harvest_Fishery2013.mat <- SEAK_StatArea_Harvest_Fishery.mat
+SEAK_StatArea_Harvest_Fishery2014.mat <- SEAK_StatArea_Harvest_Fishery.mat
+SEAK_StatArea_Harvest_Fishery2015.mat <- SEAK_StatArea_Harvest_Fishery.mat
+SEAK_StatArea_Harvest_Fishery2016.mat <- SEAK_StatArea_Harvest_Fishery.mat
+SEAK_StatArea_Harvest_Fishery2017.mat <- SEAK_StatArea_Harvest_Fishery.mat
+
+rm(SEAK_StatArea_Harvest_Fishery.mat)
+
+# Do all years have the same dimensions? No :(
+sapply(paste0("SEAK_StatArea_Harvest_Fishery", 2013:2017, ".mat"), function(yr) {dim(get(yr))} )
+yr.stat.areas <- sapply(paste0("SEAK_StatArea_Harvest_Fishery", 2013:2017, ".mat"), function(yr) {rownames(get(yr))} )
+names(yr.stat.areas) <- 2013:2017
+all.stat.areas <- unique(unlist(sapply(paste0("SEAK_StatArea_Harvest_Fishery", 2013:2017, ".mat"), function(yr) {rownames(get(yr))} )))
+
+# Prepare empty matrix
+SEAK_StatArea_Harvest_Fishery13to17.mat <- matrix(data = NA, nrow = length(all.stat.areas), 
+                                                  ncol = ncol(SEAK_StatArea_Harvest_Fishery2013.mat), 
+                                                  dimnames = list(all.stat.areas, colnames(SEAK_StatArea_Harvest_Fishery2013.mat)))
+# Fill in five year average harvest by stat area
+for(stat.area in all.stat.areas) {
+  yrs <- names(which(sapply(yr.stat.areas, function(yr) {stat.area %in% yr} )))
+  SEAK_StatArea_Harvest_Fishery13to17.mat[stat.area, ] <- colMeans(t(sapply(yrs, function(yr) {
+    get(paste0("SEAK_StatArea_Harvest_Fishery", yr, ".mat"))[stat.area, ]} 
+  )))
+}
+
+str(SEAK_StatArea_Harvest_Fishery13to17.mat)
+SEAK_StatArea_Harvest_Fishery.mat <- SEAK_StatArea_Harvest_Fishery13to17.mat
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #### Maps ####
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -101,6 +133,13 @@ SEAK_StatAreas <- StatAreas.shp@data$STAT_AREA[StatAreas.shp@data$STAT_AREA %in%
 SEAKStatAreas.shp <- subset(StatAreas.shp, StatAreas.shp@data$STAT_AREA %in% SEAK_StatAreas)
 str(SEAKStatAreas.shp, max.level = 2)
 writePolyShape(x = SEAKStatAreas.shp, fn = "GIS Data/Stat Area/SEAKStatAreas.shp")
+
+
+# Subset shapefile for just D1-4
+D1to4_StatAreas <- StatAreas.shp@data$STAT_AREA[StatAreas.shp@data$STAT_AREA %in% as.character(10000:10499)]
+D1to4StatAreas.shp <- subset(StatAreas.shp, StatAreas.shp@data$STAT_AREA %in% D1to4_StatAreas)
+str(D1to4StatAreas.shp, max.level = 2)
+writePolyShape(x = D1to4StatAreas.shp, fn = "GIS Data/Stat Area/D1to4StatAreas.shp")
 
 
 # Are all the stat areas in SEAK harvest present in the shapefile?
@@ -200,6 +239,7 @@ str(StatAreasPBS.shp, max.level = 2)
 str(attributes(StatAreasPBS.shp))
 str(attributes(StatAreasPBS.shp)[["PolyData"]])
 attributes(StatAreasPBS.shp)[["PolyData"]] <- cbind(attributes(StatAreasPBS.shp)[["PolyData"]], SEAK_StatArea_Harvest_Fishery.mat[match(as.character(attributes(StatAreasPBS.shp)[["PolyData"]]$STAT_AREA), rownames(SEAK_StatArea_Harvest_Fishery.mat)), ])
+# attributes(StatAreasPBS.shp)[["PolyData"]] <- cbind(attributes(StatAreasPBS.shp)[["PolyData"]], SEAK_StatArea_Harvest_Fishery13to17.mat[match(as.character(attributes(StatAreasPBS.shp)[["PolyData"]]$STAT_AREA), rownames(SEAK_StatArea_Harvest_Fishery13to17.mat)), ])
 attributes(StatAreasPBS.shp)[["PolyData"]][, 9:15][is.na(attributes(StatAreasPBS.shp)[["PolyData"]][, 9:15])] <- 0
 
 # Read in dsistrict shapefile
@@ -232,16 +272,16 @@ addLines(polys = RiversPBS.shp, col = "gray50", lwd = 1)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Replicate Harvest Map Function
-Plot_StatArea_SEAK_Harvest_MapPBS.f <- function(fishery, max.col = NULL) {
+Plot_StatArea_SEAK_Harvest_MapPBS.f <- function(fishery, max.col = NULL, xmin = 222.5, xmax = 230, ymin = 54.5, ymax = 59.75) {
   if(is.null(max.col)) {max.col <- max(attributes(StatAreasPBS.shp)[["PolyData"]][, fishery])}
   
-  land <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/gshhs_h.b", xlim = c(222.5, 230), ylim = c(54.5, 59.75) , maxLevel = 4, useWest = TRUE)
+  land <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/gshhs_h.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax) , maxLevel = 4, useWest = TRUE)
   plotMap(land, col = "gray90")
   
-  rivers <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_rivers_f.b", xlim = c(222.5, 230), ylim = c(54.5, 59.75), useWest = TRUE)
-  addLines(polys = rivers, col = "gray50", lwd = 2)
+  # rivers <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_rivers_f.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax), useWest = TRUE)
+  # addLines(polys = rivers, col = "gray50", lwd = 2)
   
-  borders <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_borders_f.b", xlim = c(222.5, 230), ylim = c(54.5, 59.75), useWest = TRUE)
+  borders <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_borders_f.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax), useWest = TRUE)
   addLines(polys = borders, col = "black", lwd = 2)
   
   color.ramp <- colorRampPalette(c("white", "green", "darkgreen", "black"))(101)
@@ -253,12 +293,81 @@ Plot_StatArea_SEAK_Harvest_MapPBS.f <- function(fishery, max.col = NULL) {
          legend = c(max.col, rep("", 99), 0),
          fill = rev(color.ramp),
          border = NA,
-         bty = 'n', x.intersp = 0.5, y.intersp = 0.07, lty = NULL)
-  text(x = -137.3, y = 55.65, labels = "Harvest", cex = 1.3, adj = c(0, 0.5))
-  text(x = -137.3, y = 55.8, labels = fishery, cex = 1.3, adj = c(0, 0.5))
+         bty = 'n', x.intersp = 0.5, y.intersp = 0.07, lty = NULL, cex = 0.7)
+  text(x = -137.3, y = 56.05, labels = "Harvest", cex = 1.3, adj = c(0, 0.5))  #  x = 55.65
+  text(x = -137.3, y = 56.2, labels = fishery, cex = 1.3, adj = c(0, 0.5))  # x = 55.8
 }
 
 Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Drift", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
 Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Seine", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
 Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "SHA", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Commercial", max.col = 1.5e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+
+setwd("V:/Presentations/Regional/1_SEAK/Sockeye/2017 April Port Sampler Meeting/Figures/")
+
+png(filename = "2016 Drift Harvest.png", width = 1100, height = 1400, res = 180)
+Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Drift", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+dev.off()
+
+png(filename = "2016 Seine Harvest.png", width = 1100, height = 1400, res = 180)
+Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Seine", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+dev.off()
+
+png(filename = "2016 Commercial Harvest.png", width = 1100, height = 1400, res = 180)
 Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Commercial", max.col = 2.2e5)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+dev.off()
+
+
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### PBS Mapping D1-4 ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## NOTE: It is very important that the xtent of the map shows ALL stat areas
+## If you want to restrict the map extent (zoom in), you'll need to subset the 
+## shapefile to show just the stat areas you want, otherwise you will get
+## inaccurate results. The shapes are drawn in order and will bomb out once a
+## shape is not on the extent, potentially leaving you with white space in a
+## subsequent stat area that actually had harvest!!!
+
+StatAreasPBS.shp <- importShapefile(fn = "GIS Data/Stat Area/D1to4StatAreas.shp")
+str(StatAreasPBS.shp, max.level = 2)
+str(attributes(StatAreasPBS.shp))
+str(attributes(StatAreasPBS.shp)[["PolyData"]])
+attributes(StatAreasPBS.shp)[["PolyData"]] <- cbind(attributes(StatAreasPBS.shp)[["PolyData"]], SEAK_StatArea_Harvest_Fishery.mat[match(as.character(attributes(StatAreasPBS.shp)[["PolyData"]]$STAT_AREA), rownames(SEAK_StatArea_Harvest_Fishery.mat)), ])
+# attributes(StatAreasPBS.shp)[["PolyData"]] <- cbind(attributes(StatAreasPBS.shp)[["PolyData"]], SEAK_StatArea_Harvest_Fishery13to17.mat[match(as.character(attributes(StatAreasPBS.shp)[["PolyData"]]$STAT_AREA), rownames(SEAK_StatArea_Harvest_Fishery13to17.mat)), ])
+attributes(StatAreasPBS.shp)[["PolyData"]][, 9:15][is.na(attributes(StatAreasPBS.shp)[["PolyData"]][, 9:15])] <- 0
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Replicate Harvest Map Function
+Plot_StatArea_SEAK_Harvest_MapPBS.f <- function(fishery, max.col = NULL, xmin = 222.5, xmax = 230, ymin = 54.5, ymax = 59.75) {
+  if(is.null(max.col)) {max.col <- max(attributes(StatAreasPBS.shp)[["PolyData"]][, fishery])}
+  
+  land <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/gshhs_h.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax) , maxLevel = 4, useWest = TRUE)
+  plotMap(land, col = "gray90")
+  
+  # rivers <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_rivers_f.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax), useWest = TRUE)
+  # addLines(polys = rivers, col = "gray50", lwd = 2)
+  
+  borders <- importGSHHS("C:/Users/krshedd/Documents/R/win-library/3.3/PBSmapping/wdb_borders_f.b", xlim = c(xmin, xmax), ylim = c(ymin, ymax), useWest = TRUE)
+  addLines(polys = borders, col = "black", lwd = 2)
+  
+  color.ramp <- colorRampPalette(c("white", "green", "darkgreen", "black"))(101)
+  # color.ramp <- colorRampPalette(c("white", brewer.pal(9, "Greys")))(101)
+  
+  addPolys(polys = StatAreasPBS.shp, border = "black", col = color.ramp[round(attributes(StatAreasPBS.shp)[["PolyData"]][, fishery] / (max.col/100)) + 1])
+  
+  legend("bottomleft",
+         legend = c(max.col, rep("", 99), 0),
+         fill = rev(color.ramp),
+         border = NA,
+         bty = 'n', x.intersp = 0.5, y.intersp = 0.07, lty = NULL, cex = 0.7)
+  text(x = -133.95, y = 55.05, labels = "Harvest", cex = 0.8, adj = c(0, 0.5))  #  x = 55.65
+  text(x = -133.95, y = 55.12, labels = fishery, cex = 0.8, adj = c(0, 0.5))  # x = 55.8
+}
+
+# dir.create("Figures")
+png(filename = "Figures/2013-17 Avg Commercial Harvest.png", width = 1100, height = 1400, res = 180)
+Plot_StatArea_SEAK_Harvest_MapPBS.f(fishery = "Commercial", max.col = 1.5e5, xmin = 226, ymax = 56.2)  # max(SEAK_StatArea_Harvest_Fishery.mat)
+dev.off()
